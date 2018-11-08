880b4352483c79d560362ecef394abbd54640584
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 10:29:04 +0100

    Operators I

---
+++ src/Aufgabe3_1810653153.java
@@ -0,0 +1,22 @@
+public class Aufgabe3_1810653153
+{
+    public static void main(String[] args)
+    {
+
+        int zahl1 = 24;
+        int zahl2 = 5;
+        int zahl3 = 7;
+
+        double zahl4 = 1.99;
+        double zahl5 = 2.99;
+        double zahl6 = 3.99;
+
+        System.out.println("zahl1 plus zahl2: " + (zahl1 + zahl2));
+        System.out.println("zahl1 geteilt durch zahl3: " + (zahl1 / zahl3));
+        System.out.println("zahl4 mal zahl2: " + (zahl4 * zahl2));
+        System.out.println("zahl1 modulo zahl2: " + (zahl1 % zahl2));
+        System.out.println("Inkrementieren Sie zahl3: " + (++zahl3));
+        System.out.println("Dekrementieren Sie zahl2: " + (--zahl2));
+
+    }
+}
