d1b1f092aebdb8397e7fb1d6b3c6c47e7b4f6b74
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 10:18:07 +0100

    Define data types and value range

---
+++ src/Aufgabe2_1810653153.java
@@ -0,0 +1,34 @@
+public class Aufgabe2_1810653153
+{
+        public static void main(String[] args)
+        {
+
+            // Wertebereich: Menge aller Zeichenketten
+            String a = "Manuel Seisl";
+
+            // Wertebereich: -2 147 483 648 ... 2 147 483 647
+            int b = 26;
+
+            // Wertebereich: +/-4,9E-324 ... +/-1,7E+308
+            double c = 2.99;
+
+            // Wertebereich: true, false
+            boolean d = true;
+
+            // Wertebereich: - 9 223 372 036 854 775 808 ... 9 223 372 036 854 775 807
+            long e = 20;
+
+            // Wertebereich: 0 ... 65 535
+            char f = 'A';
+
+            // Wertebereich: -128 ... 127
+            byte g = 126;
+
+            // Wertebereich: -32 768 ... 32 767
+            short h = 32766;
+
+            // Wertebereich: +/-1,4E-45 ... +/-3,4E+38
+            float i = 2.356f;
+
+        }
+}
