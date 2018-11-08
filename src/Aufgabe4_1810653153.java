e34625310a1ef68f458cf01ea37c2d41b0b3d238
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 10:35:00 +0100

    Operators II

---
+++ src/Aufgabe4_1810653153.java
@@ -0,0 +1,21 @@
+public class Aufgabe4_1810653153
+{
+    public static void main(String[] args)
+    {
+
+        int zahl1 = 33;
+        int zahl2 = 76;
+
+        System.out.println("zahl1 < zahl2: " + (zahl1 < zahl2));
+        System.out.println("zahl2 < zahl1: " + (zahl2 < zahl1));
+        System.out.println("zahl1 > zahl2: " + (zahl1 > zahl2));
+        System.out.println("zahl2 > zahl1: " + (zahl2 > zahl1));
+        System.out.println("zahl1 <= zahl2: " + (zahl1 <= zahl2));
+        System.out.println("zahl2 <= zahl1: " + (zahl2 <= zahl1));
+        System.out.println("zahl1 >= zahl2: " + (zahl1 >= zahl2));
+        System.out.println("zahl2 >= zahl1: " + (zahl2 >= zahl1));
+        System.out.println("zahl1 == zahl2: " + (zahl1 == zahl2));
+        System.out.println("zahl1 != zahl2: " + (zahl1 != zahl2));
+
+    }
+}
